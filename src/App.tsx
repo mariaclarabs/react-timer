0b36775085083896fa21bacf8da35caef8781ba7
mariaclarabs <clarabsouza@hotmail.com>
2022-09-24 22:46:02 -0300

    style: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
 
-import { defaultTheme } from "./styles/themes/default";
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
@@ -9,5 +9,5 @@ export function App() {
       <h1>Ignite Timer</h1>
       <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
